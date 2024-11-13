a4494f8fc2205a7da21a24d167059249565b320b
Dorota Stefanik <dorota.stefanik@gmail.comm>
2024-11-13 16:08:22 +0100

    Implementation with zustand.

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,8 +1,7 @@
 import Select from "react-select";
 import EmptyView from "./EmptyView";
 import { useMemo, useState } from "react";
-
-import { useItemsContext } from "../lib/hooks";
+import { useItemsStore } from "../stores/itemsStore";
 
 const sortingOptions = [
   {
@@ -20,8 +19,10 @@ const sortingOptions = [
 ];
 
 export default function ItemList() {
+  const items = useItemsStore((state) => state.items);
+  const deleteItem = useItemsStore((state) => state.deleteItem);
+  const toggleItem = useItemsStore((state) => state.toggleItem);
   const [sortBy, setSortBy] = useState("default");
-  const { items, handleDeleteItem, handleToggleItem } = useItemsContext();
 
   const sortedItems = useMemo(() =>
     [...items].sort((a, b) => {
@@ -56,8 +57,8 @@ export default function ItemList() {
           <Item
             key={item.id}
             item={item}
-            onDeleteItem={handleDeleteItem}
-            onToggleItem={handleToggleItem}
+            onDeleteItem={deleteItem}
+            onToggleItem={toggleItem}
           />
         );
       })}
