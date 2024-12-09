49889c3ab64843e386aceda18ad9c641b87da9be
Dorota Stefanik <dorota.stefanik@gmail.com>
2024-12-09 20:20:36 +0100

    Use implicit returns in store.

--- src/stores/itemsStore.js
+++ src/stores/itemsStore.js
@@ -24,37 +24,31 @@ export const useItemsStore = create(
           packed: false,
         };
 
-        set(state => {
-          const newItems = [...state.groups[state.currentGroupIndex].items, newItem];
-
-          return updateItems(state, newItems);
-        });
+        set(state => updateItems(state, [...state.groups[state.currentGroupIndex].items, newItem]));
       },
       deleteItem: id => {
-        set(state => {
-          const newItems = state.groups[state.currentGroupIndex].items.filter(item => item.id !== id);
-
-          return updateItems(state, newItems);
-        });
+        set(state =>
+          updateItems(
+            state,
+            state.groups[state.currentGroupIndex].items.filter(item => item.id !== id)
+          )
+        );
       },
       toggleItem: id => {
-        set(state => {
-          const newItems = state.groups[state.currentGroupIndex].items.map(item => {
-            if (item.id === id) {
-              return { ...item, packed: !item.packed };
-            }
-            return item;
-          });
-
-          return updateItems(state, newItems);
-        });
+        set(state =>
+          updateItems(
+            state,
+            state.groups[state.currentGroupIndex].items.map(item => {
+              if (item.id === id) {
+                return { ...item, packed: !item.packed };
+              }
+              return item;
+            })
+          )
+        );
       },
       removeAllItems: () => {
-        set(state => {
-          const newItems = [];
-
-          return updateItems(state, newItems);
-        });
+        set(state => updateItems(state, []));
       },
       resetToInitial: () => {
         set(state => {
@@ -81,8 +75,8 @@ export const useItemsStore = create(
   )
 );
 
-function markAllAs(state, complete) {
-  const newItems = state.groups[state.currentGroupIndex].items.map(item => ({ ...item, packed: complete }));
-
-  return updateItems(state, newItems);
-}
+const markAllAs = (state, complete) =>
+  updateItems(
+    state,
+    state.groups[state.currentGroupIndex].items.map(item => ({ ...item, packed: complete }))
+  );
